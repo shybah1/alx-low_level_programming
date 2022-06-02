8bfccb8b914721a458c91476cb36e3327b6c192f
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-06-02 07:06:09 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point 
- * use function printf
- * Return: Always 0 (Success)
+ * main - Entry point
+ * use printf function
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, ");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art, ")
+	return (0)
 }

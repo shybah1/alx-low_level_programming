8f8c81e55c0e2b7671a0f6c9305f022b8c2df555
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-06-02 09:07:26 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	Char str1[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str1[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	return (1);
 }

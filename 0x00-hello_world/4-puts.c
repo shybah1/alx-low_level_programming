7b20c37310bd423f997203cedbb95378e5c27c09
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-06-02 05:36:31 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,7 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle")
-	return (0)
+{	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

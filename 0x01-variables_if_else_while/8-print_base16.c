b4ebc82d052c16277a69ede93ff85c7d3af28e8a
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-06-03 17:19:29 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,12 +2,12 @@
 /**
  * main - Prints numbers from 0 to 9, and alphabets from a to f.
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
-	
+
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);

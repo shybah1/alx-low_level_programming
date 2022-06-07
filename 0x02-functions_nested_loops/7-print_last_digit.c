b7cc1a92cebe6094ff2c09b572305ae018efc06c
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-06-07 13:26:46 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,12 +13,12 @@ int print_last_digit(int n)
 	l = n % 10;
 	if (l < 0)
 	{
-		_putchar(-1 + 48);
-		return (-1);
+		_putchar(-l + 48);
+		return (-l);
 	}
 	else
 	{
-		_putchar(1 + 48);
-		return (1);
+		_putchar(l + 48);
+		return (l);
 	}
 }

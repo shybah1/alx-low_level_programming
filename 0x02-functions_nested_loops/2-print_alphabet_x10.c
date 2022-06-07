842e309321a7ef8a1407c38e16bde4ddae5868e6
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-06-07 08:34:37 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include<main.h>
+/**
+ * print_alphabet_x10 - print the alphabets 10 time
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+
+	_put_char('\n');
+	}
+}

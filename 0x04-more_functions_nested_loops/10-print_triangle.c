0462fb13fd046cc123a859fbcc1f90a33a124917
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-06-09 10:02:55 -0700

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,8 +10,8 @@ void print_triangle(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (i = 1; j < (size - i); j++)
-			_putchar('#');
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
 		for (j--; j < size; j++)
 			_putchar(35);
 		if (i < (size - 1))

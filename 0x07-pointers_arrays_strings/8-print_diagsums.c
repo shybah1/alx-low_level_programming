c5fc7fb6bd7fd8f7cc96841e15cc6a4d70465cd2
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-06-20 08:08:57 -0700

    chessboard

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,7 +3,8 @@
 /**
  * print_diagsums - prints the sum of the two diagonals 
  * of a square matrix of integers.
- * @a: size of the matrix
+ * @a: input pointer
+ * @size: size of the matrix
  * Return: no return.
  */
 void print_diagsums(int *a, int size)

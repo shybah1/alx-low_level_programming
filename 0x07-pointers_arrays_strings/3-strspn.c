bee56685c79ca55ffbc0472207b891de3efc8773
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-06-20 05:34:46 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		bool = 1;
-		for (j == 0; *(accept + j) != '\0'; j++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 			{

3f6b2ecf315ef2d9e7b0c9d543d5b9b744bdade3
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-06-24 06:40:12 -0700

    arg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,13 +3,13 @@
  * main - prints the number of arguments passed into it.
  * @argc: number of command line arguments.
  * @argv: array containing the index program command line arguments.
- * Return: 0 success.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int j;
+	int i;
 
-	for (j = 0; j < argc; j++)
-		printf("%s\n", argv[j]);
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }

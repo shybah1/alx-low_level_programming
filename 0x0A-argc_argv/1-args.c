58c5c9c20b20d7b99e9fb4e153a147daf12a71ce
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-06-24 07:04:18 -0700

    arg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,11 +5,8 @@
  * @argv: array containing the index program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
-	return (0);
+		printf("%d\n", argc - 1);
+		return (0);
 }

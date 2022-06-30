e652bf645b857d4fbdd1088363688598d5f3db20
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-06-30 05:17:24 -0700

    malloc checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 /**
  * malloc_checked - to allocates memory.
- * @a: amount of bytes.
+ * @b: amount of bytes.
  *
  * Return: pointer to the allocated memory.
  * if malloc fails, status value is equal to 98.
  */
-void *malloc_checked(unsigned int a)
+void *malloc_checked(unsigned int b)
 {
 	char *p;
 
 
-	p = malloc(a);
+	p = malloc(b);
 	if (p == NULL)
 		exit(98);
 	return (p);
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,6 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - to concatenates two strings.
+ * @s1: first string.

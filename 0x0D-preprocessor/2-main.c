ab9c5c31ad890db80d40b7e25756f3d5ab030c27
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-07-04 06:01:56 -0700

    main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - prints the name of the file it was compiled from.
  *
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	printf("%s/n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }

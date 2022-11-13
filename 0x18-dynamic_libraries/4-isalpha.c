de6c27fde58ace1fd70bc398bde00b9e31f2a63c
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-11-13 14:46:23 -0800

    makefiles

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - prints 1 if an input is a letter, 0 if otherwise.
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer.
+ *
+ * @i: input an integer number
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
